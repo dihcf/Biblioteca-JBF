d1507ef9664b8b3372e1d711ae7f9ec8f9c46a16
Diego Cavalcante <infoalanp@gmail.com>
2025-05-12 14:28:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 let modalOne = document.getElementById("return")
-let Devolucao = document.getElementById("devolucao")
-let close = document.getElementById("close-modal")
+let modalTwo = document.getElementById("modal-edit")
+let devolucao = document.getElementById("devolucao")
+let close = document.querySelectorAll(".close-modal")
 let search = document.getElementById("input_search")
-let a
- 
+let data = document.querySelectorAll(".conteinerData")
 let count = document.querySelectorAll(".conteinerData").length
+let a
+let addNome = document.getElementById("add-nome")
+let addLivro = document.getElementById("add-livro")
 
 devolucao.addEventListener("click",() =>{
   modalOne.style.display = "flex"
 })
 
-close.addEventListener("click",()=>{
-    modalOne.style.display = "none"
-})
 
+close.forEach((teste)=>{
+    teste.addEventListener("click", ()=>{
+        if(teste.id == "oneC"){
+            modalOne.style.display = "none"
+        } else if(teste.id == "twoC"){
+            modalTwo.style.display = "none"
+        }
+    })
+    
+})
+    
 search.addEventListener("input",(e)=>{
         let campoE = e.target.value.toLowerCase()   
         for(a=0;a<count;a++){
@@ -21,12 +32,28 @@ search.addEventListener("input",(e)=>{
             if(text.includes(campoE)){
                 let camposData = document.querySelectorAll(".conteinerData")
                 camposData[a].style.display = "flex"
-            
             } else {
                 let camposData = document.querySelectorAll(".conteinerData")
                 camposData[a].style.display = "none"
             }
 
        }
+})   
+
+data.forEach((item) => {
+    item.addEventListener("click", () => {
+        
+        let textos = item.querySelectorAll("p")
+        let nomeAluno = textos[0].textContent
+        let nomeLivro = textos[2].textContent
+
+        
+        addNome.textContent = "Aluno: " + nomeAluno
+        addLivro.textContent = "Livro: " + nomeLivro
+
+       
+        modalTwo.style.display = "flex"
+    })
 })
+
 
